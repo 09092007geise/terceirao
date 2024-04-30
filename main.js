29929e94015fc0365dae078dd329c435f2251523
09092007geise <105931122+09092007geise@users.noreply.github.com>
2024-04-30 11:26:35 -0300

    z

--- main.js
+++ main.js
@@ -14,9 +14,10 @@ for (let i = 0; i < botoes.length; i++){
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date ("2024-12-01T23:59:59");
 const tempoObjetivo2 = new Date ("2024-07-01T23:59:59");
-const tempoObjetivo3 = new Date ("2024-03-01T23:59:59");
+const tempoObjetivo3 = new Date ("2024-08-01T23:59:59");
 const tempoObjetivo4 = new Date ("2024-12-01T23:59:59");
-const tempoObjetivo4 = new Date ("2024-23-01T23:59:59");
+const tempoObjetivo5 = new Date ("2024-12-01T23:59:59");
+
 const tempos = [
     tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4
 ]
